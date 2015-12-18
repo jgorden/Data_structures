51cd8450ff6641a1b94dc96bb4fda0c099df6341
jgorden <joji.gorden@gmail.com>
2015-12-18 12:25:16 -0800

    add fixed array

---
+++ fixed_aray.rb
@@ -0,0 +1,24 @@
+class FixedArray
+	attr_reader :array
+
+	def new(size)
+		@array = Array.new(size)
+	end
+
+	def get(index)
+		raise 'OutOfBoundsException' if index > @array.length - 1
+		@array[index]
+	end
+
+	def set(index, element)
+		raise 'OutOfBoundsException' if index > @array.length - 1
+		@array[index] = element
+	end
+end
+
+a = FixedArray.new
+a.new(3)
+p a.array
+a.set(0, 'hey')
+p a.get(0)
+p a.array
