e45f50690e32728ff3000601000a5a23d310b8b1
jgorden <joji.gorden@gmail.com>
2015-12-19 15:46:00 -0800

    add linkedlist class to linked_list

--- linked_list.rb
+++ linked_list.rb
@@ -1,4 +1,5 @@
 class Node
+  attr_reader :value, :next
   def initialize(element)
     @value = element
     @next = nil
@@ -13,3 +14,51 @@ class Node
   end
 end
 
+class LinkedList
+  def initialize()
+    @head = nil
+  end
+
+  def insert_first(node)
+    node.insert_after(@head)
+    @head = node
+  end
+
+  def remove_first
+    @head = @head.next
+  end
+
+  def insert_last(node)
+    if @head == nil
+      @head = node
+    else
+      current = @head
+      while current.next != nil
+        current = current.next
+      end
+      current.insert_after(node)
+    end
+  end
+
+  def remove_last
+    return nil if @head == nil
+    if @head.next == nil
+      @head = nil
+    else
+      current = @head
+      while current.next.next != nil
+        current = current.next
+      end
+      current.remove_after
+    end
+  end
+end
+
+a = Node.new('a')
+b = Node.new('b')
+c = Node.new('c')
+x = LinkedList.new
+x.insert_first(a)
+x.insert_first(b)
+x.insert_last(c)
+p x
