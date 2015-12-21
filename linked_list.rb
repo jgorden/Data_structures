fb4f51a7f74099904add6b5ae23c462680000bb5
jgorden <joji.gorden@gmail.com>
2015-12-20 16:22:01 -0800

    add get, set, and size methods on LinkedList class

--- linked_list.rb
+++ linked_list.rb
@@ -52,6 +52,41 @@ class LinkedList
       current.remove_after
     end
   end
+
+  def get(index)
+    count = 0
+    current = @head
+    until count == index
+      current = current.next
+      count += 1
+    end
+    current.value
+  end
+
+  def set(index, element)
+    if index == 0
+      insert_first(element)
+    else
+      count = 0
+      current = @head
+      until count == index - 1
+        current = current.next
+        count += 1
+      end
+      current.insert_after(element)
+    end
+  end
+
+  def size
+    return 0 if @head == nil
+    count = 1
+    current = @head
+    until current.next == nil
+      count += 1
+      current = current.next
+    end
+    count
+  end
 end
 
 a = Node.new('a')
@@ -61,4 +96,7 @@ x = LinkedList.new
 x.insert_first(a)
 x.insert_first(b)
 x.insert_last(c)
-p x
+p x.get(0)
+# p x.set(0,c)
+# p x.get(0)
+p x.size
